127278bc480e7336ef74fba9cdad8fee8c1a113a
Rémi Marseille <marseille.remi@gmail.com>
2013-10-12 20:08:52 +0200

    Added copyrights

--- src/Mremi/UrlShortener/Provider/Bitly/AuthenticationInterface.php
+++ src/Mremi/UrlShortener/Provider/Bitly/AuthenticationInterface.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Mremi\UrlShortener library.
+ *
+ * (c) Rémi Marseille <marseille.remi@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Mremi\UrlShortener\Provider\Bitly;
 
 /**
